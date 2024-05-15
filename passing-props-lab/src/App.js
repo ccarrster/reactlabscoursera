73b0023b4421a830cc8bbf4aef64a889177a0ed8
Chris <chris.carr@cobersolutions.com>
2024-05-15 16:50:27 -0400

    Adding labs

---
+++ passing-props-lab/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Heading from './Heading';
+
+function App() {
+  return (
+    <div className="App">
+      <Heading firstName="Bob" />
+      <Heading firstName="Other Bob" />
+    </div>
+  );
+}
+
+export default App;
